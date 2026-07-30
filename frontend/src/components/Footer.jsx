5f3c9d74b8002a97092c7d7a059cb24c3cd4b8ce
sanjidaahmed120803 <sanjidaahmed12.08.03@gmail.com>
2026-07-30 15:39:45 +0600

    Create footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        textAlign: "center",
+        padding: "20px",
+        background: "#1f2937",
+        color: "white",
+        marginTop: "40px",
+      }}
+    >
+      <p>© 2026 RecycleHub. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
