30b8abd4db65ba079cec0e0c3b6734c0379f5141
sanjidaahmed120803 <sanjidaahmed12.08.03@gmail.com>
2026-08-03 22:01:47 +0600

    Create products page

---
+++ frontend/js/products.js
@@ -0,0 +1,27 @@
+const searchInput = document.getElementById("searchInput");
+
+searchInput.addEventListener("keyup", function () {
+
+    const value = searchInput.value.toLowerCase();
+
+    const cards = document.querySelectorAll(".product-card");
+
+    cards.forEach(card => {
+
+        const text = card.innerText.toLowerCase();
+
+        if(text.includes(value)){
+
+            card.style.display="block";
+
+        }
+
+        else{
+
+            card.style.display="none";
+
+        }
+
+    });
+
+});
